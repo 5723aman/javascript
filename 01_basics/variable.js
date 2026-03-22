fc3889749f8eb0a8b6a4fff774d2c92b845c1789
Aman Kumar Singh <152841289+5723aman@users.noreply.github.com>
2026-03-22 18:51:09 +0000

    config comit update

---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = 1223
+let accountEmail = "aman@gmail.com"
+var accountPassword= "1234"
+accountCity = "Ara"
+
+/*
+prefer not to use var because of issue  in block scope and functional scope
+*/
